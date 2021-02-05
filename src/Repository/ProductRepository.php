6726dbba32b75ef84ba47e34d154f436e1e0ae5d
zoran <zoran_gogusev@yahoo.com>
2021-02-05 12:18:46 +0100

    Create dinamic custom display of "you may also like this" section for products

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +18,15 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    public function findYouMyAlsoLikeProducts($mainProduct)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.id != ' . $mainProduct)
+            ->getQuery()
+            ->getResult(Query::HYDRATE_ARRAY)
+            ;
     }
 
     // /**
